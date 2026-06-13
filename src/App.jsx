3274109f710f26f471740b45aac8be9eaf11652b
Jacelikas-create <holfeljr@mail.uc.edu>
2026-06-12 23:59:31 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ErrandsPanel from "./components/ErrandsPanel";
 import WeightTab from "./components/WeightTab";
 import WorkoutTracker from "./components/WorkoutTracker";
 import FoodTab from "./components/FoodTab";
-import ParTab from "./components/ParTab.jsx";
+import ParTab from "./components/ParTab";
 
 export default function App() {
   const todayCode = getTodayCode();
